c0c19f93557550cc00cbf81fbd2d7fbee0f37a5d
Mysticalai <mysticalai@tutanota.com>
2024-01-12 19:00:45 +0800

    Revert "Update checkpoints.cpp"
    This reverts commit f84219caa478693830791acb9f5bac9df996e019.

--- src/checkpoints/checkpoints.cpp
+++ src/checkpoints/checkpoints.cpp
@@ -184,15 +184,21 @@ namespace cryptonote
   {
     if (nettype == TESTNET)
     {
-     // ADD_CHECKPOINT2(0,     "48ca7cd3c8de5b6a4d53d2861fbdaedca141553559f9be9520068053cda8430b", "0x1")
+<<<<<<< 8430b", "0x1")
+=======
+      //ADD_CHECKPOINT2(0,     "48ca7cd3c8de5b6a4d53d2861fbdaedca141553559f9be9520068053cda8430b", "0x1");
+
       return true;
     }
     if (nettype == STAGENET)
     {
+<<<<<<< HEAD
      // ADD_CHECKPOINT2(0,       "76ee3cc98646292206cd3e86f74d88b4dcc1d937088645e9b0cbca84b7ce74eb", "0x1")
+
       return true;
     }
-    //ADD_CHECKPOINT2(1,     "771fbcd656ec1464d3a02ead5e18644030007a0fc664c0a964d30922821a8148", "0x2")
+    ADD_CHECKPOINT2(0,     "771fbcd656ec1464d3a02ead5e18644030007a0fc664c0a964d30922821a8148", "0x1");
+
     return true;
   }
 
@@ -237,7 +243,10 @@ namespace cryptonote
     std::vector<std::string> records;
 
     // All four LunexaPulse domains have DNSSEC on and valid
-    static const std::vector<std::string> dns_urls = { ""
+    static const std::vector<std::string> dns_urls = { "checkpoints.moneropulse.se"
+						     , "checkpoints.moneropulse.org"
+						     , "checkpoints.moneropulse.net"
+						     , "checkpoints.moneropulse.co"
     };
 
     static const std::vector<std::string> testnet_dns_urls = { "testpoints.moneropulse.se"
