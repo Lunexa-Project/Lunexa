3731acab9d80ac53e93864e352a385e1e9bfefa6
Lunexa-Project <156077973+Lunexa-Project@users.noreply.github.com>
2024-10-05 00:43:56 +0800

    Updating for Bootstrappable Builds

--- contrib/depends/builders/darwin.mk
+++ contrib/depends/builders/darwin.mk
@@ -1,65 +1,22 @@
-OSX_MIN_VERSION=10.13
-OSX_SDK_VERSION=11.0
-XCODE_VERSION=12.2
-XCODE_BUILD_ID=12B45b
-LD64_VERSION=609
+build_darwin_CC: = $(shell xcrun -f clang)
+build_darwin_CXX: = $(shell xcrun -f clang++)
+build_darwin_AR: = $(shell xcrun -f ar)
+build_darwin_RANLIB: = $(shell xcrun -f ranlib)
+build_darwin_STRIP: = $(shell xcrun -f strip)
+build_darwin_OTOOL: = $(shell xcrun -f otool)
+build_darwin_NM: = $(shell xcrun -f nm)
+build_darwin_INSTALL_NAME_TOOL:=$(shell xcrun -f install_name_tool)
+build_darwin_SHA256SUM = shasum -a 256
+build_darwin_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --retry $(DOWNLOAD_RETRIES) -o
 
-OSX_SDK=$(host_prefix)/native/SDK
-
-darwin_native_toolchain=darwin_sdk native_cctools
-
-clang_prog=$(shell $(SHELL) $(.SHELLFLAGS) "command -v clang")
-clangxx_prog=$(shell $(SHELL) $(.SHELLFLAGS) "command -v clang++")
-
-# Flag explanations:
-#
-#     -mlinker-version
-#
-#         Ensures that modern linker features are enabled. See here for more
-#         details: https://github.com/bitcoin/bitcoin/pull/19407.
-#
-#     -B$(build_prefix)/bin
-#
-#         Explicitly point to our binaries (e.g. cctools) so that they are
-#         ensured to be found and preferred over other possibilities.
-#
-#     -isysroot$(OSX_SDK) -nostdlibinc
-#
-#         Disable default include paths built into the compiler as well as
-#         those normally included for libc and libc++. The only path that
-#         remains implicitly is the clang resource dir.
-#
-#     -iwithsysroot / -iframeworkwithsysroot
-#
-#         Adds the desired paths from the SDK
-#
-
-darwin_CC=env -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH \
-              -u OBJC_INCLUDE_PATH -u OBJCPLUS_INCLUDE_PATH -u CPATH \
-              -u LIBRARY_PATH \
-            $(clang_prog) --target=$(host) -mmacosx-version-min=$(OSX_MIN_VERSION) \
-              -B$(build_prefix)/bin -mlinker-version=$(LD64_VERSION) \
-              -isysroot$(OSX_SDK) \
-              -isysroot$(OSX_SDK) -nostdlibinc \
-              -iwithsysroot/usr/include -iframeworkwithsysroot/System/Library/Frameworks
-
-darwin_CXX=env -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH \
-               -u OBJC_INCLUDE_PATH -u OBJCPLUS_INCLUDE_PATH -u CPATH \
-               -u LIBRARY_PATH \
-             $(clangxx_prog) --target=$(host) -mmacosx-version-min=$(OSX_MIN_VERSION) \
-               -B$(build_prefix)/bin -mlinker-version=$(LD64_VERSION) \
-               -isysroot$(OSX_SDK) -nostdlibinc \
-               -iwithsysroot/usr/include/c++/v1 \
-               -iwithsysroot/usr/include -iframeworkwithsysroot/System/Library/Frameworks
-
-darwin_CFLAGS=-pipe
-darwin_CXXFLAGS=$(darwin_CFLAGS)
-darwin_ARFLAGS=cr
-
-darwin_release_CFLAGS=-O2
-darwin_release_CXXFLAGS=$(darwin_release_CFLAGS)
-
-darwin_debug_CFLAGS=-O1
-darwin_debug_CXXFLAGS=$(darwin_debug_CFLAGS)
-
-darwin_cmake_system=Darwin
+#darwin host on darwin builder. overrides darwin host preferences.
+darwin_CC=$(shell xcrun -f clang) -mmacosx-version-min=$(OSX_MIN_VERSION)
+darwin_CXX:=$(shell xcrun -f clang++) -mmacosx-version-min=$(OSX_MIN_VERSION) -stdlib=libc++
+darwin_AR:=$(shell xcrun -f ar)
+darwin_RANLIB:=$(shell xcrun -f ranlib)
+darwin_STRIP:=$(shell xcrun -f strip)
+darwin_LIBTOOL:=$(shell xcrun -f libtool)
+darwin_OTOOL:=$(shell xcrun -f otool)
+darwin_NM:=$(shell xcrun -f nm)
+darwin_INSTALL_NAME_TOOL:=$(shell xcrun -f install_name_tool)
+darwin_native_toolchain=
