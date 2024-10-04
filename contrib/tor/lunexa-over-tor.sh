19e51eed7b2ab26b8e478318a646bf83a789108a
Lunexa-Project <156077973+Lunexa-Project@users.noreply.github.com>
2024-10-04 20:07:54 +0800

    Updating Depends for GUIX Bootstrappable build
    all based on tobtoht GUIX setup for monero

---
+++ contrib/tor/lunexa-over-tor.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+DIR=$(realpath $(dirname $0))
+
+echo "Checking lunexad..."
+lunexad=""
+for dir in \
+  . \
+  "$DIR" \
+  "$DIR/../.." \
+  "$DIR/build/release/bin" \
+  "$DIR/../../build/release/bin" \
+  "$DIR/build/Linux/master/release/bin" \
+  "$DIR/../../build/Linux/master/release/bin" \
+  "$DIR/build/Windows/master/release/bin" \
+  "$DIR/../../build/Windows/master/release/bin"
+do
+  if test -x "$dir/lunexad"
+  then
+    lunexad="$dir/lunexad"
+    break
+  fi
+done
+if test -z "$lunexad"
+then
+  echo "lunexad not found"
+  exit 1
+fi
+echo "Found: $lunexad"
+
+TORDIR="$DIR/lunexa-over-tor"
+TORRC="$TORDIR/torrc"
+HOSTNAMEFILE="$TORDIR/hostname"
+echo "Creating configuration..."
+mkdir -p "$TORDIR"
+chmod 700 "$TORDIR"
+rm -f "$TORRC"
+cat << EOF > "$TORRC"
+ControlSocket $TORDIR/control
+ControlSocketsGroupWritable 1
+CookieAuthentication 1
+CookieAuthFile $TORDIR/control.authcookie
+CookieAuthFileGroupReadable 1
+HiddenServiceDir $TORDIR
+HiddenServicePort 19023 127.0.0.1:19023
+EOF
+
+echo "Starting Tor..."
+nohup tor -f "$TORRC" 2> "$TORDIR/tor.stderr" 1> "$TORDIR/tor.stdout" &
+ready=0
+for i in `seq 10`
+do
+  sleep 1
+  if test -f "$HOSTNAMEFILE"
+  then
+    ready=1
+    break
+  fi
+done
+if test "$ready" = 0
+then
+  echo "Error starting Tor"
+  cat "$TORDIR/tor.stdout"
+  exit 1
+fi
+
+echo "Starting lunexad..."
+HOSTNAME=$(cat "$HOSTNAMEFILE")
+"$lunexad" \
+  --anonymous-inbound "$HOSTNAME":19023,127.0.0.1:19023,25 --tx-proxy tor,127.0.0.1:9050,10 \
+
+  --detach
+ready=0
+for i in `seq 10`
+do
+  sleep 1
+  status=$("$lunexad" status)
+  echo "$status" | grep -q "Height:"
+  if test $? = 0
+  then
+    ready=1
+    break
+  fi
+done
+if test "$ready" = 0
+then
+  echo "Error starting lunexad"
+  tail -n 400 "$HOME/.lunexa/bitlunexa.log" | grep -Ev stacktrace\|"Error: Couldn't connect to daemon:"\|"src/daemon/main.cpp:.*lunexa\ \'" | tail -n 20
+  exit 1
+fi
+
+echo "Ready. Your Tor hidden service is $HOSTNAME"
