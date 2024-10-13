b7215a02fd379a5584d1e65e941fe2c0817d4947
Lunexa-Project <156077973+Lunexa-Project@users.noreply.github.com>
2024-10-13 17:29:08 +0800

    updating epee

--- contrib/epee/src/mlocker.cpp
+++ contrib/epee/src/mlocker.cpp
@@ -41,8 +41,8 @@
 
 #include <atomic>
 
-#undef LUNEXA_DEFAULT_LOG_CATEGORY
-#define LUNEXA_DEFAULT_LOG_CATEGORY "mlocker"
+#undef MONERO_DEFAULT_LOG_CATEGORY
+#define MONERO_DEFAULT_LOG_CATEGORY "mlocker"
 
 // did an mlock operation previously fail? we only
 // want to log an error once and be done with it
--- contrib/epee/src/network_throttle-detail.cpp
+++ contrib/epee/src/network_throttle-detail.cpp
@@ -59,8 +59,8 @@
 // TODO:
 #include "net/network_throttle-detail.hpp"
 
-#undef LUNEXA_DEFAULT_LOG_CATEGORY
-#define LUNEXA_DEFAULT_LOG_CATEGORY "net.throttle"
+#undef MONERO_DEFAULT_LOG_CATEGORY
+#define MONERO_DEFAULT_LOG_CATEGORY "net.throttle"
 
 // ################################################################################################
 // ################################################################################################
