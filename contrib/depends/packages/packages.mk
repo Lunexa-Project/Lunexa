e9d0082aa28abd851733c2fa5cee25380be5d7e7
Lunexa-Project <156077973+Lunexa-Project@users.noreply.github.com>
2024-10-20 22:14:24 +0800

    Update packages.mk

--- contrib/depends/packages/packages.mk
+++ contrib/depends/packages/packages.mk
@@ -1,24 +1,19 @@
 packages:=boost openssl zeromq expat unbound sodium
 
-# ccache is useless in gitian builds
-ifneq ($(GITIAN),1)
-native_packages := native_ccache
-endif
-
 hardware_packages := hidapi protobuf libusb
 hardware_native_packages := native_protobuf
 
 android_native_packages = android_ndk $(hardware_native_packages)
-android_packages = ncurses readline sodium protobuf
+android_packages = ncurses readline protobuf
 
 darwin_native_packages = $(hardware_native_packages)
-darwin_packages = ncurses readline sodium $(hardware_packages)
+darwin_packages = ncurses readline $(hardware_packages)
 
 # not really native...
 freebsd_native_packages = freebsd_base $(hardware_native_packages)
-freebsd_packages = ncurses readline sodium protobuf libusb
+freebsd_packages = ncurses readline protobuf libusb
 
-linux_packages = eudev ncurses readline sodium $(hardware_packages)
+linux_packages = eudev ncurses readline  $(hardware_packages)
 linux_native_packages = $(hardware_native_packages)
 
 ifeq ($(build_tests),ON)
